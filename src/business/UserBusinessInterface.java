d2770a310663b1363d7bdb2266bc203f3bd4dc83
JoshVandeWalle <joshv98@icloud.com>
2021-04-10 15:04:02 -0700

    testing

--- src/business/UserBusinessInterface.java
+++ src/business/UserBusinessInterface.java
@@ -3,6 +3,11 @@ package business;
 import models.Credentials;
 import models.User;
 
+/**
+ * This interface defines the business logic API for users
+ * @author t34su
+ *
+ */
 public interface UserBusinessInterface {
 	public int register(User record);
 	public User authenticate(Credentials credentials);
